bc96b9ae042c41ac3ef8400b9d3c026f6fb0ba46
aaa <aaa@gmail.com>
2025-04-18 15:14:36 +0300

    dfdf

--- js/data.js
+++ js/data.js
@@ -1,7 +1,8 @@
 export default [
   {
-    title: "Ikkichi oy 9-dars",
-    github: "https://github.com/oybekovichweb/2.9-dars",
-    vercel: "https://2-9-dars.vercel.app/",
+    title: "Namanganliklar24",
+    github: "https://https://github.com/oybekovichweb/imtihon-figma",
+    vercel: "https://imtihon-figma-alpha.vercel.app/",
   },
+
 ];
