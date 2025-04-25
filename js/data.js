26898009ca86ad44ea813398f0724470a877f2f4
aaa <aaa@gmail.com>
2025-04-25 17:50:14 +0300

    .

--- js/data.js
+++ js/data.js
@@ -3,6 +3,10 @@ export default [
     title: "Namanganliklar24",
     github: "https://github.com/oybekovichweb/imtihon-figma",
     vercel: "https://imtihon-figma-alpha.vercel.app/",
+  }, {
+    title: "MYteam",
+    github: "https://github.com/oybekovichweb/my-team-new",
+    vercel: "https://my-team-new-gold.vercel.app/",
   },
 
 ];
