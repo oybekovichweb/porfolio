77b8ad8827e7ef58a9d444d07c3685a1ac51c453
aaa <aaa@gmail.com>
2025-05-16 19:02:59 +0300

    fiigmalarni qoshdim

--- js/data.js
+++ js/data.js
@@ -1,12 +1,31 @@
 export default [
   {
-    title: "Namanganliklar24",
-    github: "https://github.com/oybekovichweb/imtihon-figma",
-    vercel: "https://imtihon-figma-alpha.vercel.app/",
+    title: "namangan 24",
+    github: "https://github.com/oybekovichweb/Namangan-24",
+    vercel: "https://namangan-24-eight.vercel.app/",
   }, {
     title: "MYteam",
     github: "https://github.com/oybekovichweb/my-team-new",
     vercel: "https://my-team-new-gold.vercel.app/",
-  },
-
+  }, {
+    title: "figma 4 dars",
+    github: "https://github.com/oybekovichweb/figma-4",
+    vercel: "https://figma-4-seven.vercel.app/",
+  }, {
+    title: "3.6dars birinchi vazifam",
+    github: "https://github.com/oybekovichweb/3.6",
+    vercel: "https://3-6-three.vercel.app/",
+  }, {
+    title: "3.6dars ikkinchi vazifam",
+    github: "https://github.com/oybekovichweb/3.6.2chi-vazifam",
+    vercel: "https://3-6-2chi-vazifam-c4z5.vercel.app/",
+  }, {
+    title: "Tech book",
+    github: "https://github.com/oybekovichweb/scss-template",
+    vercel: "https://scss-template-5b89.vercel.app/"
+    }, {
+      title: "ikkinchi figma imtihon",
+      github: "https://github.com/oybekovichweb/scss-template",
+      vercel: "https://scss-template-urnz.vercel.app/"
+      },
 ];
